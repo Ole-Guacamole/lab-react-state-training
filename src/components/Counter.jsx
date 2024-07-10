ede4abd6ae9515b421a245050b394755f0ed554a
Sven Ole Spindler <s.spindler@gmx.de>
2024-07-10 17:44:16 +0200

    solved Problem with coun <= 0

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,11 +5,12 @@ function Counter() {
 
   return (
     <div className="Counter">
-     
       <button onClick={() => setCount(count + 1)}> + </button>
       <p>{count}</p>
-      <button onClick={() => setCount(count - 1)}> - </button>
-      
+      <button onClick={() => setCount(count - 1)} disabled={count <= 0}>
+        {" "}
+        -{" "}
+      </button>
     </div>
   );
 }
