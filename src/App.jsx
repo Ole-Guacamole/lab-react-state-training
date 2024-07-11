94dddd72651f592fb1af93926726e1a414390c66
Sven Ole Spindler <s.spindler@gmx.de>
2024-07-11 10:10:20 +0200

    implement carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import ClickablePicture from "./components/ClickablePicture";
 import Counter from "./components/Counter";
 import LikeButton from "./components/LikeButton";
 import Dice from "./components/Dice";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
       <h2>Iteration 1: Like Button</h2>
-      <LikeButton/>
+      <LikeButton />
       <h2>Iteration 2 | Counter</h2>
-      <Counter/>
+      <Counter />
       <h2>Iteration 3 | State: ClickablePicture</h2>
-      <ClickablePicture/>
+      <ClickablePicture />
       <h2>Iteration 4 | State: Dice</h2>
-      <Dice/>
+      <Dice />
+      <h2>Bonus | Carousel</h2>
+      <Carousel />
     </div>
   );
 }
